b9dbb8785a2ded1acda87f19088a8473b90ca127
xmavv <xxmatheooo@gmail.com>
2023-12-25 17:41:28 +0100

    js creates list of items each type

---
+++ src/script.js
@@ -0,0 +1,121 @@
+const dataJson = [
+    {
+      "title": "work",
+      "timeframes": {
+        "daily": {
+          "current": 5,
+          "previous": 7
+        },
+        "weekly": {
+          "current": 32,
+          "previous": 36
+        },
+        "monthly": {
+          "current": 103,
+          "previous": 128
+        }
+      }
+    },
+    {
+      "title": "play",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 2
+        },
+        "weekly": {
+          "current": 10,
+          "previous": 8
+        },
+        "monthly": {
+          "current": 23,
+          "previous": 29
+        }
+      }
+    },
+    {
+      "title": "study",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 7
+        },
+        "monthly": {
+          "current": 13,
+          "previous": 19
+        }
+      }
+    },
+    {
+      "title": "exercise",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 5
+        },
+        "monthly": {
+          "current": 11,
+          "previous": 18
+        }
+      }
+    },
+    {
+      "title": "social",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 3
+        },
+        "weekly": {
+          "current": 5,
+          "previous": 10
+        },
+        "monthly": {
+          "current": 21,
+          "previous": 23
+        }
+      }
+    },
+    {
+      "title": "self-care",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 2,
+          "previous": 2
+        },
+        "monthly": {
+          "current": 7,
+          "previous": 11
+        }
+      }
+    }
+];
+
+const list = document.querySelector('.details__list');
+
+list.innerHTML = dataJson.map(action => {
+    return `<li class="details__item details__item--${action["title"]}">
+                            <div class="details__item-info">
+                                <p class="details__item-action">${action["title"]}</p>
+                                <img src="./../images/icon-ellipsis.svg" alt="" class="details__item-more">
+                                <h2 class="details__item-hours">${action["timeframes"]["daily"]["current"]}hrs</h2>
+                                <span class="details__item-last">Last week - ${action["timeframes"]["daily"]["previous"]}hrs</span>
+                            </div>
+
+                            <div class="details__item-bg">
+                                <img src="./../images/icon-${action["title"]}.svg" alt="" class="details__item-img">
+                            </div>
+                        </li>`;
+}).join('');

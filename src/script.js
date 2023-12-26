e535621b39f6fa2944d964b453d23fa97a488d81
xmavv <xxmatheooo@gmail.com>
2023-12-26 03:22:54 +0100

    info changing on click, js functions

--- src/script.js
+++ src/script.js
@@ -100,22 +100,51 @@ const dataJson = [
           "previous": 11
         }
       }
-    }
+    },
+    // {
+    //   "title": "hello",
+    //   "timeframes": {
+    //     "daily": {
+    //       "current": 5,
+    //       "previous": 5
+    //     },
+    //     "weekly": {
+    //       "current": 5,
+    //       "previous": 5
+    //     },
+    //     "monthly": {
+    //       "current": 5,
+    //       "previous": 5
+    //     }
+    //   }
+    // }
 ];
 
-const list = document.querySelector('.details__list');
+function renderItems(time){
+  const list = document.querySelector('.details__list');
+  list.innerHTML = dataJson.map(action => {
+      return `<li class="details__item details__item--${action["title"]}" id="${action["title"]}">
+                              <div class="details__item-info">
+                                  <p class="details__item-action">${action["title"][0].toUpperCase() + action["title"].substring(1)}</p>
+                                  <img src="./../images/icon-ellipsis.svg" alt="" class="details__item-more">
+                                  <h2 class="details__item-hours">${action["timeframes"][time]["current"]}hrs</h2>
+                                  <span class="details__item-last">Last week - ${action["timeframes"][time]["previous"]}hrs</span>
+                              </div>
 
-list.innerHTML = dataJson.map(action => {
-    return `<li class="details__item details__item--${action["title"]}">
-                            <div class="details__item-info">
-                                <p class="details__item-action">${action["title"]}</p>
-                                <img src="./../images/icon-ellipsis.svg" alt="" class="details__item-more">
-                                <h2 class="details__item-hours">${action["timeframes"]["daily"]["current"]}hrs</h2>
-                                <span class="details__item-last">Last week - ${action["timeframes"]["daily"]["previous"]}hrs</span>
-                            </div>
+                              <div class="details__item-bg">
+                                  <img src="./../images/icon-${action["title"]}.svg" alt="" class="details__item-img">
+                              </div>
+                          </li>`;
+  }).join('');
+}
 
-                            <div class="details__item-bg">
-                                <img src="./../images/icon-${action["title"]}.svg" alt="" class="details__item-img">
-                            </div>
-                        </li>`;
-}).join('');
+function showInfo(){
+  renderItems(this.dataset.name);
+  userAction.forEach(action => action.classList.remove('active'));
+  this.classList.add('active');
+}
+
+renderItems("daily");
+
+const userAction = [...document.querySelector('.user__action').children];
+userAction.forEach(action => action.addEventListener('click', showInfo));

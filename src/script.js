3cfd33ac0e2325f2a9e83fedf3015fb22cc4b55d
xmavv <xxmatheooo@gmail.com>
2023-12-28 18:02:44 +0100

    ::before pseudoelement on moreIcon click

--- src/script.js
+++ src/script.js
@@ -16,24 +16,31 @@ async function renderItems(time){
   if(time === "monthly") last ="month";
   list.innerHTML = data.map(action => {
       return `<li class="details__item details__item--${action["title"]}" id="${action["title"]}">
-                              <div class="details__item-info">
-                                  <p class="details__item-action">${action["title"][0].toUpperCase() + action["title"].substring(1).replace('-', ' ')}</p>
-                                  <img src="./images/icon-ellipsis.svg" alt="" class="details__item-more">
-                                  <h2 class="details__item-hours">${action["timeframes"][time]["current"]}hrs</h2>
-                                  <span class="details__item-last">Last ${last} - ${action["timeframes"][time]["previous"]}hrs</span>
-                              </div>
+                  <div class="details__item-info">
+                      <p class="details__item-action">${action["title"][0].toUpperCase() + action["title"].substring(1).replace('-', ' ')}</p>
+                      <input type="checkbox" id="icon${action["title"]}"></input>
+                      <label for="icon${action["title"]}"><img src="./images/icon-ellipsis.svg" alt="" class="details__item-more"></label>
+                      <h2 class="details__item-hours">${action["timeframes"][time]["current"]}hrs</h2>
+                      <span class="details__item-last">Last ${last} - ${action["timeframes"][time]["previous"]}hrs</span>
+                  </div>
 
-                              <div class="details__item-bg">
-                                  <img src="./images/icon-${action["title"]}.svg" alt="" class="details__item-img">
-                              </div>
-                          </li>`;
+                  <div class="details__item-bg">
+                      <img src="./images/icon-${action["title"]}.svg" alt="" class="details__item-img">
+                  </div>
+              </li>`;
   }).join('');
 
   const moreIcons = document.querySelectorAll('.details__item-more');
   moreIcons.forEach(icon => {
     icon.addEventListener('mouseenter', changeIcon);
     icon.addEventListener('mouseleave', changeIcon);
-  }); 
+  });
+
+  const inputs = document.querySelectorAll('input');
+  console.log(inputs);
+  inputs.forEach(input => input.addEventListener('click', function() {
+    console.log(this.checked);
+  }));
 }
 
 function showInfo(){
